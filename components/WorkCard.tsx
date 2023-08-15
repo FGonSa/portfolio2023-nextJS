a86edf1b52d3fee032f316283f3e00df3cd2b26a
Frank Santos <fransecre@gmail.com>
2023-08-15 21:45:17 +0200

    WorkCard corregido

--- components/WorkCard.tsx
+++ components/WorkCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Tooltip } from 'react-tooltip'
@@ -14,14 +15,14 @@ function WorkCard({ work, url }: Props) {
     <>
       <Link href={`/works/${work.url}`}>
         <div className="w-72 h-full rounded-b overflow-hidden shadow-lg bg-slate-100 hover:bg-slate-200">
-          <img className="w-full" src={work.pic} alt={work.name} />
+          <Image className="w-full" src={work.pic} alt={work.name} />
           <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">{work.name}</div>
             <p className="text-gray-700 text-base">{work.description}</p>
           </div>
           <div className="px-6 pt-4 pb-2">
             {work.categories.map((category) => (
-              <span className="inline-block bg-amber-500  px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              <span key={category} className="inline-block bg-amber-500  px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
                 {category}
               </span>
             ))}
@@ -29,10 +30,11 @@ function WorkCard({ work, url }: Props) {
               {work.icons.map((icon) => (
                 <div data-tooltip-id="my-tooltip" data-tooltip-content={icon.name}>
                   <Tooltip id="my-tooltip" />
-                  <img
+                  <Image
                     className="w-10 h-10 rounded-full mr-4"
                     src={icon.url}
                     alt="icon"
+                    key={icon.url}
                   />
               </div>
               ))}
