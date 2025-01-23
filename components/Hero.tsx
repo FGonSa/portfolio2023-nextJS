9f1d679b2f5b0651257df9d0de90c14e0a2584cd
Francisco Javier González <78041280+FGonSa@users.noreply.github.com>
2025-01-23 19:54:41 +0100

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -37,7 +37,7 @@ export default function Hero() {
 
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
-                  href="/cv_eng.pdf"
+                  href="/CV.pdf"
                   className="rounded-md bg-yellow-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-pink-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Download CV
